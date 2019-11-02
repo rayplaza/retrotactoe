7ecef148a91db04ff4d55c1e9282a5f34f95f962
Ray Plaza <munditoplaza@gmail.com>
2019-11-02 12:29:43 -0700

    pushing original complete code with some css  changes

--- js/app.js
+++ js/app.js
@@ -1,17 +1,31 @@
 /*----- constants -----------------------------------------------------*/
-const PLAYER_1_WIN = 3;
-const PLAYER_2_WIN = -3;
-// const COLOR {
-//     playerOne: blue,
-//     playerTwo: green,
-//     emptySquare: grey
-// }
-
-
+const winCombos = [ 
+   [0,1,2],
+   [3,4,5],
+   [6,7,8],
+   [0,3,6],
+   [1,4,7],
+   [2,5,8],
+   [0,4,8],
+   [2,4,6]
+];
 
 
 /*----- app's state (variables) --------------------------------------*/
-let turn, winner, board;
+function init() {
+   turn = 1;
+   board = [0,0,0,
+            0,0,0,
+            0,0,0]
+   squares.forEach(function(e){
+       e.textContent = '';
+       e.className = 'box';
+   });
+
+   boxContainer.addEventListener('click', handleClick)
+   messageEl.textContent = '';
+   }
+
 
 
 
@@ -19,25 +33,20 @@ let turn, winner, board;
 
 /*----- cached element references ------------------------------------*/
 const messageEl = document.querySelector('h2');
-const boardEl = document.querySelector('section')
-const resetButton = document.getElementById('reset')
-let boardContainer = [[0, 0, 0],
-                      [0, 0, 0],
-                      [0, 0, 0]];
-
-
-for (c=0; c < 3; c++) {
-   for (r=0; r < 3; r++) {
-   boardContainer[c][r] = document.getElementById(`c${c}r${r}`)
-   }
-};
+const resetEl = document.getElementById('reset');
+const boxContainer = document.querySelector('.container')
+const squares = document.querySelectorAll('div')
 
 
 
 
 
 /*---------- event listeners ------------------------------------------*/
-document.getElementById('')
+resetEl.addEventListener('click', init)
+
+ boxContainer.addEventListener('click', handleClick)
+
+init();
 
 
 
@@ -46,6 +55,43 @@ document.getElementById('')
 
 /*----------- functions ----------------------------------------------*/
 
-function handleSquareClick(evt) {
-
+function handleClick(evt) {
+   if (evt.target.tagName !== 'DIV' || evt.target.className === 'disable') {
+   return;
+   }
+   board[parseInt(evt.target.id)] = turn;
+   console.log(turn)
+   if(turn === 1) {
+       
+       evt.target.textContent = 'X'
+   } else {
+       evt.target.textContent = 'O'
+   }
+   evt.target.className = 'disable'
+   checkWinner();
+   turn *= -1;
 }
+
+
+function checkWinner() {
+   for (i=0; i < winCombos.length; i++) {
+       let counter = 0;
+           for(f=0; f < 3; f++) {
+               counter += board[winCombos[i][f]]
+           }
+       if (counter === 3 || counter === -3) {
+           if (counter === 3) {
+           messageEl.textContent = "X is the winner winner chicken dinner!!!"
+           boxContainer.removeEventListener('click', handleClick)
+           } else if (counter === -3) {
+               messageEl.textContent = "O is the winner winner chicken dinner!!!"
+               boxContainer.removeEventListener('click', handleClick)
+           }
+           return;
+       } else if (!board.includes(0)) {
+           messageEl.textContent = "Tie Game!"
+           return;
+       }
+   }
+}    
+
